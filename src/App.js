546fc6338f4aba1eca1fe9cb18e951243ab555f1
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-05 12:10:06 +0530

    Update App.js with protected routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "./App.css"
+import { useState } from "react";
 
-const App = () => (
-  <Router>
-    <div>
-      <h1>MERN Auth</h1>
-      <nav>
-        <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
-      </nav>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </div>
-  </Router>
-);
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
+  const ProtectedRoute = ({ element }) => {
+    if (!isAuthenticated) {
+      return <Navigate to="/login" />;
+    }
+    return element;
+  };
+
+  return (
+    <Router>
+      <div>
+        <h1>MERN Auth</h1>
+        <nav>
+          <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+        </nav>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute element={<Home />} />
+            }
+          />
+          <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
