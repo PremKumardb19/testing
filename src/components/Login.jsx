8224ff7174005973334bb80ed4ea3d93401a08db
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-05 12:03:46 +0530

    Update Login.jsx with css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom"
+import "../App.css"
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -18,7 +19,7 @@ const Login = () => {
   };
 
   return (
-    <div>
+    <div className="auth-container">
       <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
       <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
