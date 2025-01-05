cac7ef1c1623c1826dff39fe389cfd20e940ec77
PremKumar <143885129+PremKumardb19@users.noreply.github.com>
2025-01-05 12:02:05 +0530

    Update App.js to add css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css"
 
 const App = () => (
   <Router>
